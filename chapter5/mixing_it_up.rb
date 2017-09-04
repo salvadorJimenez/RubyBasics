0289b54e67953ebaa40076ad817798a81fdca5c3
Salvador Jiménez Mendoza <sjimenez.developer@gmail.com>
2017-09-04 16:41:13 -0500

    five chapter problems

---
+++ chapter5/mixing_it_up.rb
@@ -0,0 +1,7 @@
+print "What is your name? "
+name=gets.chomp
+print "What is your middle name? "
+middle=gets.chomp
+print "What is your last name? "
+last=gets.chomp
+puts "#{name} #{middle} #{last} is a great name"
