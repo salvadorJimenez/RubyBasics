9fdce7a6836078f2704e45bac352fca9f2593933
Salvador Jiménez Mendoza <sjimenez.developer@gmail.com>
2017-09-04 16:57:29 -0500

    some corrections

--- chapter1/numbers.rb
+++ chapter1/numbers.rb
@@ -1,10 +1,10 @@
 puts "How many hours are in a year?"
-hours_in_year=24*365
+hours_in_year=24 * 365
 puts ">>> There are #{hours_in_year} hours"
 puts "How many minutes are in a decade?"
-minutes_in_decade=((24*365)*10)*60
+minutes_in_decade=((24 * 365) * 10) * 60
 puts ">>> There are #{minutes_in_decade} minutes"
 print "How old are you? "
 age=gets.chomp.to_i
-seconds_in_life=((24*365)*3600)*age
+seconds_in_life=((24 * 365) * 3600)*age
 puts "You has lived #{seconds_in_life} seconds"
--- chapter5/mixing_it_up.rb
+++ chapter5/mixing_it_up.rb
@@ -8,4 +8,4 @@ puts "#{name} #{middle} #{last} is a great name"
 puts "----------------------------------"
 print "Please enter your favorite number: "
 number=gets.chomp.to_i
-puts "I think that #{number+1} is a better number"
+puts "I think that #{number + 1} is a better number"
