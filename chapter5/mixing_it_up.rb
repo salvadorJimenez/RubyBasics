79b66a1d1e83bfeb706cf7a590f8d225b40b3cbf
Salvador Jiménez Mendoza <sjimenez.developer@gmail.com>
2017-09-04 16:52:28 -0500

    five chapter problems 2.0

--- chapter5/mixing_it_up.rb
+++ chapter5/mixing_it_up.rb
@@ -5,3 +5,7 @@ middle=gets.chomp
 print "What is your last name? "
 last=gets.chomp
 puts "#{name} #{middle} #{last} is a great name"
+puts "----------------------------------"
+print "Please enter your favorite number: "
+number=gets.chomp.to_i
+puts "I think that #{number+1} is a better number"
